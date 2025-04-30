2c43b1cde6d24244532f9623f8784dcf331cb022
Your Name <you@example.com>
2025-04-30 14:23:43 +0200

    accedemos a los datos del WS

--- src/js/script.js
+++ src/js/script.js
@@ -5,20 +5,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function obtenerDatos() {
     fetch(
-        "http://httpserverjson.v1.localhost/index.php"
+        "http://httpserverjson.v1.localhost/ws/json1.php"
     ) // URL de ejemplo con JSON
-        .then(response => response.json())
-        .then(data => mostrarDatos(data))
-        .catch(error => console.error("Error al obtener datos:", error));
+    .then(response => response.json())
+    .then(data => mostrarDatos(data))
+    .catch(error => console.error("Error al obtener datos:", error));
 }
 
 function mostrarDatos(datos) {
     const contenedor = document.getElementById("data-container");
     contenedor.innerHTML = ""; // Limpiar contenido previo
 
-    datos["menu"].forEach((elemento) => {
+    Object.keys(datos["menu"]).forEach((clave) => {
         const div = document.createElement("div");
-        div.innerHTML = `<strong>${elemento.name}</strong> - ${elemento.email} (${usuario.address.city})`;
+        div.innerHTML += `<strong>${clave} - ${datos["menu"][clave]}</strong>`;
         contenedor.appendChild(div);
     });
 }
