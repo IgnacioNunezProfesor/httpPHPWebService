86e4e07cb69fb87911b74887fb69a48b74585b68
Ignacio Núñez <inr.profesor@outlook.es>
2025-06-25 19:19:42 +0200

    Limpiamos directorios

--- docker/db/mongodb/mongodb.dockerfile
+++
@@ -1,3 +0,0 @@
-FROM mongo:latest
-COPY ./db/mongodb/loadFiles.sh /docker-entrypoint-initdb.d/
-EXPOSE 27017
--- scripts/buildDEVApachephp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker-compose --env-file ./env/.dev.env -f ./docker/docker-compose.apachephp.build.dev.yml up --build -d apachephp
